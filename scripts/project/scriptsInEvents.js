d866d4b73b6934feb8fd55dc8b1eb8b19bd77420
Dev33IO <127096734+Dev33IO@users.noreply.github.com>
2023-07-08 00:58:20 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,51 @@
+
+
+
+const scriptsInEvents = {
+
+	async Egame_Event1_Act1(runtime, localVars)
+	{
+		screen = 3;
+		
+	},
+
+	async Egame_Event1_Act2(runtime, localVars)
+	{
+		localStorage.setItem('screen', '3');
+	},
+
+	async Egame_Event1_Act7(runtime, localVars)
+	{
+		localStorage.setItem('color', '#fcf99b');
+	},
+
+	async Egame_Event1_Act10(runtime, localVars)
+	{
+		runtime.globalVars.per2 = localStorage.getItem('screenshot2');
+	},
+
+	async Egame_Event8_Act1(runtime, localVars)
+	{
+		runtime.globalVars.per = localStorage.getItem('screenshot');
+	},
+
+	async Egame_Event16_Act2(runtime, localVars)
+	{
+		localStorage.setItem('color', runtime.globalVars.HEXcolor);
+	},
+
+	async Egame_Event18_Act2(runtime, localVars)
+	{
+		screen = 3;
+		
+	},
+
+	async Egame_Event18_Act3(runtime, localVars)
+	{
+		localStorage.setItem('screen', '3');
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
